3813caf76b2611648da6445e349c708c719a925f
Lawrence Yoon <yoonlawrence@yahoo.com>
2021-06-10 23:07:32 -0700

    finished read for specific id, and added validation

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,5 +1,22 @@
 const productsService = require('./products.service');
 
+// validation
+function productExists(req, res, next) {
+  const { productId } = req.params;
+
+  productsService
+    .read(productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
+}
+
+// handlers
 function list(req, res, next) {
   productsService
     .list()
@@ -7,6 +24,11 @@ function list(req, res, next) {
     .catch(next);
 }
 
+function read(req, res) {
+  res.json({ data: res.locals.product });
+}
+
 module.exports = {
+  read: [productExists, read],
   list,
 };
