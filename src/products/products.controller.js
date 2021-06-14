871a80c2475568fadf0201391a51d6623081f29a
Lawrence Yoon <yoonlawrence@yahoo.com>
2021-06-14 12:35:55 -0700

    working on aggregate data

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,34 +1,38 @@
 const productsService = require('./products.service');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
 // validation
-function productExists(req, res, next) {
+async function productExists(req, res, next) {
   const { productId } = req.params;
+  const product = await productsService.read(productId);
 
-  productsService
-    .read(productId)
-    .then((product) => {
-      if (product) {
-        res.locals.product = product;
-        return next();
-      }
-      next({ status: 404, message: `Product cannot be found.` });
-    })
-    .catch(next);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Product cannot be found.`,
+  });
 }
 
 // handlers
-function list(req, res, next) {
-  productsService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(req, res, next) {
+  const data = await productsService.list();
+
+  res.json({ data });
 }
 
 function read(req, res) {
   res.json({ data: res.locals.product });
 }
 
+async function listOutOfStockCount(req, res, next) {
+  res.json({ data: await productsService.listOutOfStockCount() });
+}
+
 module.exports = {
-  read: [productExists, read],
-  list,
+  read: [asyncErrorBoundary(productExists), read],
+  list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
 };
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -8,7 +8,16 @@ function list() {
   return knex('products').select('*');
 }
 
+function listOutOfStockCount() {
+  return knex('products')
+    .select('product_quantity_in_stock as out_of_stock')
+    .count('product_id')
+    .where({ product_quantity_in_stock: 0 })
+    .groupBy('out_of_stock');
+}
+
 module.exports = {
   read,
   list,
+  listOutOfStockCount,
 };
